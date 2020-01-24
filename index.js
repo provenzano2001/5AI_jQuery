bbf37b61cbe1bfe85e30a54f59e78dafb56a8919
Provenzano Riccardo <provenzano.riccardo@osdbsesto>
2020-01-24 10:03:07 +0100

    add button and btn.eventListener

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 document.getElementById("h1").innerHTML = "ciao come stai";
+var btn = document.createElement("button");
+btn.innerHTML = "CLICCA";
+
+var body = document.getElementsByTagName("body")[0];
+body.append(btn);
+
+btn.addEventListener("click", function() {
+    document.getElementById("h1").innerHTML = "ciao come stai?";
+});
